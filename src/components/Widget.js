efb5fd7ef462157104cc17c34996da3f71766e59
AndreyKonovalenko <andreinew@mail.ru>
2023-01-06 00:51:16 +0300

    chore: syling

--- src/components/Widget.js
+++ src/components/Widget.js
@@ -1,63 +1,6 @@
-import { Children } from 'react';
 import uniqid from 'uniqid';
 
 const Widget = () => {
-  const styles = {
-    ticketContainer: {
-      marginTop: '15px',
-      marginLeft: '15px',
-      position: 'absolute',
-      left: '0%',
-      right: '0%',
-      top: '0%',
-      bottom: '0%',
-      fontFamily: 'Roboto',
-      fontStyle: 'normal',
-      fontWeight: '400',
-      fontSize: '19px',
-      lineHeight: '115%',
-      color: '#72ED93',
-    },
-    dateContainer: {
-      marginTop: '15px',
-      marginRight: '15px',
-      position: 'absolute',
-      left: '-1.18%',
-      right: '0%',
-      top: '0%',
-      bottom: '0%',
-      fontFamily: 'inherit',
-      fontStyle: 'normal',
-      fontWeight: '400',
-      fontSize: '19px',
-      lineHeight: '115%',
-      color: '#FFFFFF',
-      textAlign: 'right',
-    },
-    intervalBar: {
-      paddingLeft: '15px',
-      marginTop: '268px',
-      display: 'flex',
-      flexDirection: 'row',
-      alignItems: 'flex-start',
-      height: '40px',
-      background: '#141414',
-      borderRadius: '0px 0px 10px 10px',
-      flex: 'none',
-      order: '1',
-      alignSelf: 'stretch',
-      flexGrow: '0',
-    },
-    intervalStyle: {
-      margin: '10px 10px 10px 0px',
-      fontSize: '17px',
-      fontWeight: '400',
-      color: '#8D8D8D',
-      fontFamily: 'inherit',
-      fontStyle: 'normal',
-    },
-  };
-
   const PriceChart = (props) => {
     const { children } = props;
     const styles = {
@@ -65,7 +8,7 @@ const Widget = () => {
         display: 'flex',
         flexDirection: 'column',
         justifyContent: 'center',
-        alignItems: 'flexStart',
+        alignItems: 'flex-start',
         padding: '0px',
         position: 'absolute',
         width: '510px',
@@ -128,17 +71,59 @@ const Widget = () => {
     };
     return (
       <div style={styles.headerItem}>
-        <p style={styles.headerItemText}>{children}</p>
+        <span style={styles.headerItemText}>{children}</span>
       </div>
     );
   };
-  const intervals = ['Time', '15m', '1H', '4H', '1D', '1W'];
 
-  // const data = [];
-  // intervals.forEach((element) => {
-  //   data.push(<Interval key={uniqid()}>{element}</Interval>);
-  // });
-  // console.log(typeof data);
+  const TimeSwitch = () => {
+    const styles = {
+      timeSwitch: {
+        display: 'flex',
+        flexDirection: 'row',
+        alignItems: 'flex-start',
+        padding: '0px 15px',
+        height: '40px',
+        background: '#141414',
+        borderRadius: '0px 0px 10px 10px',
+        flex: 'none',
+        order: '1',
+        alignSelf: 'stretch',
+        flexGrow: '0',
+      },
+    };
+    const intervals = ['Time', '15m', '1H', '4H', '1D', '1W'];
+    const data = intervals.map((element) => (
+      <TimePicker key={uniqid()}>{element}</TimePicker>
+    ));
+    return <div style={styles.timeSwitch}>{data}</div>;
+  };
+  const TimePicker = (props) => {
+    const { children } = props;
+    const styles = {
+      timePicker: {
+        display: 'flex',
+        flexDirection: 'row',
+        alignItems: 'flex-start',
+        padding: '10px 5px',
+        gap: '10px',
+        color: '#898989',
+      },
+      timePickerText: {
+        fontFamily: 'Roboto',
+        fontStyle: 'normal',
+        fontWeight: '400',
+        fontSize: '17px',
+        inlineHeight: '120%',
+      },
+    };
+    return (
+      <div style={styles.timePicker}>
+        <span style={styles.timePickerText}>{children}</span>
+      </div>
+    );
+  };
+
   const widget = (
     <PriceChart>
       <Display>
@@ -147,15 +132,7 @@ const Widget = () => {
           <HeaderItem color={'#FFFFFF'}>23 Septemper 13:00</HeaderItem>
         </Header>
       </Display>
-      {/* <div style={styles.ticketContainer}> BTC/USDT Price Chart</div>
-      <div style={styles.dateContainer}> 23 Septemper 13:00</div> */}
-      <div style={styles.intervalBar}>
-        {intervals.map((element) => (
-          <div key={uniqid()} style={styles.intervalStyle}>
-            {element}
-          </div>
-        ))}
-      </div>
+      <TimeSwitch />
     </PriceChart>
   );
 
