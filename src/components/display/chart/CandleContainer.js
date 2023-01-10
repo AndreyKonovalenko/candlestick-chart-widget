3eb2189d574b26f90ef2ce86b2d8ad3a57ae05ed
AndreyKonovalenko <andreinew@mail.ru>
2023-01-11 00:14:34 +0300

    chore: canvas stucture complited

--- src/components/display/chart/CandleContainer.js
+++ src/components/display/chart/CandleContainer.js
@@ -1,50 +1,46 @@
-import { useRef, useEffect } from "react";
+import { useRef, useEffect } from 'react';
 const CandleContainer = (props) => {
   const canvasRef = useRef(null);
   const { spread, open, high, low, close } = props;
-  const styles = {
-    candleContainer: {
-      width: "15px",
-      height: "115px",
-      flex: "none",
-      order: "1",
-      flexGrow: "0",
-    },
-  };
+  // const styles = {
+  //   candleContainer: {
+  //     width: '15px',
+  //     height: '115px',
+  //     flex: 'none',
+  //     order: '1',
+  //     flexGrow: '0',
+  //   },
+  // };
 
   const step = spread.spread / 115; // means canvas height in px =  115px
-  console.log(spread.max, spread.min, spread.spread, high, step);
-  const yLine0 = (spread.max - high) / step;
-  const yLine1 = (spread.max - low) / step;
+  const yLine0 = Math.round((spread.max - high) / step);
+  const yLine1 = Math.round((spread.max - low) / step);
 
   // open price >= close price
 
-  const yRect0 =
-    open >= close ? (spread.max - open) / step : (spread.max - close) / step;
-  const yRect1 =
-    open >= close ? (spread.max - close) / step : (spread.max - open) / step;
+  const yRect0 = Math.round(
+    open >= close ? (spread.max - open) / step : (spread.max - close) / step
+  );
+
+  const rectHeight = Math.round(
+    open >= close ? (open - close) / step : (close - open) / step
+  );
+  console.log(yLine1, rectHeight);
 
   useEffect(() => {
     const draw = (ctx) => {
-      ctx.strokeStyle = open >= close ? "#72ED93" : "#BC1C34";
-
+      ctx.strokeStyle = open >= close ? '#72ED93' : '#BC1C34';
       ctx.lineWidth = 1;
       ctx.beginPath();
-      ctx.moveTo(4, yLine0);
-      ctx.lineTo(4, yRect0);
-
-      ctx.moveTo(0, yRect0);
-      ctx.lineWidth = 7;
-      ctx.lineTo(0, yRect1);
-      ctx.lineWidth = 1;
-      ctx.moveTo(4, yRect1);
-      ctx.lineTo(4, yLine1);
-
+      ctx.moveTo(3.5, yLine0);
+      ctx.lineTo(3.5, yLine1);
       ctx.stroke();
+      ctx.fillStyle = open >= close ? '#72ED93' : '#BC1C34';
+      ctx.fillRect(0, yRect0, 7, rectHeight > 0 ? rectHeight : 1);
     };
 
     const canvas = canvasRef.current;
-    const context = canvas.getContext("2d");
+    const context = canvas.getContext('2d');
     draw(context);
   });
 
@@ -52,7 +48,7 @@ const CandleContainer = (props) => {
     // <div style={styles.candleContainer}>
     //   <canvas ref={canvasRef} width="15" height="115"></canvas>
     // </div>
-    <canvas ref={canvasRef} width="7" height="115"></canvas>
+    <canvas ref={canvasRef} width='7' height='115'></canvas>
   );
 };
 export default CandleContainer;
--- src/components/display/chart/ChartContainer.js
+++ src/components/display/chart/ChartContainer.js
@@ -2,14 +2,14 @@ const ChartContainer = (props) => {
   const { children } = props;
   const styles = {
     chartContainer: {
-      display: "flex",
-      flexDirection: "row",
-      justifyContent: "space-between",
-      width: "480px",
-      height: "115px",
-      flex: "none",
-      order: "1",
-      flexGrow: "0",
+      display: 'flex',
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+      width: '480px',
+      height: '115px',
+      flex: 'none',
+      order: '1',
+      flexGrow: '0',
     },
   };
   return <div style={styles.chartContainer}>{children}</div>;
