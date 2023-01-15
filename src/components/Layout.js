594489c06a6fd500c9e861038e5e19d83e6fbeb2
AndreyKonovalenko <andreinew@mail.ru>
2023-01-15 19:22:54 +0300

    chore: added links

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,12 +1,8 @@
+import { ReactComponent as Vector } from './assets/vetor.svg';
 const Layout = (props) => {
   const { isMobile, children } = props;
   const styles = {
     layout: {
-      // position: 'fixed',
-      // top: '-50%',
-      // left: '-50%',
-      // width: '200%',
-      // height: '200%',
       backgroundImage: `url(img/${isMobile ? 'Medium' : 'Large'}.jpg)`,
       backgroundPosition: 'center',
       backgroundSize: 'cover',
@@ -22,29 +18,50 @@ const Layout = (props) => {
       flexDirection: 'column',
       alignItems: 'flex-start',
       padding: '15px 10px',
-      gap: '2px',
     },
     link: {
+      display: 'flex',
+      flexDirection: 'row',
+      alignItems: 'flex-start',
+      gap: '2px',
+      textDecoration: 'none',
+    },
+    text: {
       fontFamily: 'Raleway',
       fontStyle: 'normal',
       fontWeight: '600',
       fontSize: '19px',
       lineHeight: '130%',
       color: '#FFFFFF',
-      textDecoration: 'none',
+    },
+    vectorContainer: {
+      display: 'flex',
+      width: '24px',
+      height: '24px',
+      alignItems: 'center',
+    },
+    vector: {
+      height: '58%',
     },
   };
 
   return (
     <div style={styles.layout}>
       {children}
-      {/* //   <img style={styles.backgroundImage} src='img/Medium.jpg' alt='' /> */}
       <div style={styles.linkContainer}>
         <a style={styles.link} href='https://github.com/AndreyKonovalenko'>
-          Carated by Andrey Konovalenko
+          <span style={styles.text}>Carated by Andrey Konovalenko</span>
+          <div style={styles.vectorContainer}>
+            <Vector style={styles.vector} />
+          </div>
         </a>
         <a style={styles.link} href='https://casetech.ru/candlestick-chart'>
-          Based on "KeйсТех" free candlestick chart case
+          <span style={styles.text}>
+            Based on "KeйсТех" free candlestick chart case
+          </span>
+          <div style={styles.vectorContainer}>
+            <Vector style={styles.vector} />
+          </div>
         </a>
       </div>
     </div>
